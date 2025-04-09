e4c07ed74ef20adabad8904ca2b3e5606c529fc5
khanmatin900 <matimkhan480@gmail.com>
2025-04-09 22:14:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function imgBtn() {
+  document.getElementById("image").style.display="Block";
+} 
+
+function imgBtn2() {
+  document.getElementById("image2").style.display="Block";
+} 
+
+function imgBtn3() {
+  document.getElementById("image3").style.display="Block";
+} 
+
+function imgBtn4() {
+  document.getElementById("image4").style.display="Block";
+} 
